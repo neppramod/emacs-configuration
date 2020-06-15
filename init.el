2e9c452e0cf5bb01faa34fccc9c843e200182eef
Pramod <neppramod@gmail.com>
2020-06-14 19:00:47 -0600

    Add init file and configuration.org file

---
+++ init.el
@@ -0,0 +1,18 @@
+
+;; All the settings and package installation is set in configuration.org
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; Anything below this is auto generated
+;; Don't edit this
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (key-chord counsel swiper avy org-bullets muban))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
