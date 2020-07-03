09c2e9b96d623c82cdb4871b94e63ba9c8916fb8
Pramod <neppramod@gmail.com>
2020-07-03 12:52:37 -0600

    Remove macros file

--- init.el
+++ init.el
@@ -1,12 +1,8 @@
-
 ;; All the settings and package installation is set in configuration.org
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;; Before loading following file (custom.org), make sure you have configured custom.org first
-;(org-babel-load-file "~/.emacs.d/custom.org")
-
-;; Load macros file. After defining macro, open below file and add macro to proper section with M-x insert-kbd-macro for future use
-(org-babel-load-file "~/.emacs.d/macros.org")
+(org-babel-load-file "~/.emacs.d/custom.org")
 
 ;; Anything below this is auto generated
 ;; Don't edit this
@@ -21,4 +17,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((((class color) (min-colors 89)) (:foreground "#839496" :background "#002b36")))))
