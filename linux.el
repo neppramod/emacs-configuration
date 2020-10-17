db81686f8898c6c7adaf9370b06e5063915d06d4
Pramod <neppramod@gmail.com>
2020-10-17 13:27:23 -0600

    Add enable-file-watchers nil and other lsp settings

--- linux.el
+++ linux.el
@@ -1,3 +1,4 @@
 (setq BIN_DIRECTORY "/usr/bin/")
 (setq MPLAYER_DIRECTORY_LOCATION BIN_DIRECTORY)
 (setq ASPELL_BIN (concat BIN_DIRECTORY "aspell"))
+(setenv "JAVA_HOME" "/usr/lib/jvm/java-14-openjdk-amd64")
