46f8dd274f037e43db62a3aec34514a37642f6ad
Pramod <neppramod@gmail.com>
2020-10-29 18:11:05 -0600

    Add changes to variables

--- linux.el
+++ linux.el
@@ -1,3 +1,5 @@
+(setq ORG_DIR "~/Dropbox/org/")
+(setq MUSIC_DIR "~/Music/")
 (setq BIN_DIRECTORY "/usr/bin/")
 (setq MPLAYER_DIRECTORY_LOCATION BIN_DIRECTORY)
 (setq ASPELL_BIN (concat BIN_DIRECTORY "aspell"))
--- mac.el
+++ mac.el
@@ -1,4 +1,5 @@
-(exec-path-from-shell-initialize)
+(setq ORG_DIR "")
+(setq MUSIC_DIR "")
 (setq BIN_DIRECTORY "/usr/local/bin/")
 (setq MPLAYER_DIRECTORY_LOCATION BIN_DIRECTORY)
 (setq ASPELL_BIN (concat BIN_DIRECTORY "aspell"))
