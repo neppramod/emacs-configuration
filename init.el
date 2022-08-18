e8fb06aa67f4352efd4e1bcc5fcfe55bff765a97
Pramod <neppramod@gmail.com>
2022-08-17 20:20:06 -0600

    Add new setup folders

--- init.el
+++ init.el
@@ -1,22 +1,27 @@
 ;; Create a variable to indicate where emacs's configuration is installed
 (setq EMACS_DIR "~/.emacs.d/")
+(setq EMACS_CONFIG_DIR (concat EMACS_DIR "configuration/"))
+(setq EMACS_PACKAGES_DIR (concat EMACS_CONFIG_DIR "packages/"))
 
 ;; Avoid garbage collection at statup
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
       gc-cons-percentage 0.6)
 
 ;; All the settings and package installation is set in configuration.org
-(org-babel-load-file "~/.emacs.d/configuration.org")
+(org-babel-load-file (concat EMACS_CONFIG_DIR "configuration.org"))
+
+;; Cleanup
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216 ; 16mb
+          gc-cons-percentage 0.1)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" default))
- '(package-selected-packages
-   '(realgud multiple-cursors lsp-java which-key lsp-mode evil expand-region solo-jazz-theme quickrun yasnippet-snippets yasnippet magit projectile helm use-package exec-path-from-shell doom-themes))
- '(warning-suppress-log-types '((comp))))
+ '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
