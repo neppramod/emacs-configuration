9f264a23e41b9a87dde24243fb0f1df7d3a016f7
Pramod <neppramod@gmail.com>
2022-08-17 18:01:04 -0600

    Add current settings

--- init.el
+++ init.el
@@ -7,3 +7,19 @@
 
 ;; All the settings and package installation is set in configuration.org
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" default))
+ '(package-selected-packages
+   '(realgud multiple-cursors lsp-java which-key lsp-mode evil expand-region solo-jazz-theme quickrun yasnippet-snippets yasnippet magit projectile helm use-package exec-path-from-shell doom-themes))
+ '(warning-suppress-log-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
