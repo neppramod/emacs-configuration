583f356fd9a561812d05e5fd0848ed65644e70ec
Pramod <neppramod@gmail.com>
2022-08-19 06:16:44 -0600

    Add which key and projectile global-key C-c p

--- init.el
+++ init.el
@@ -20,9 +20,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4cc1cc7efd5c2362ef684657eec7d7e482223b1def4edeb0fab52ba1d334d38a" default))
+   '("0c2d7f410f835d59a0293f2a55744e9d3be13aab8753705c6ad4a9a968fb3b28" "4cc1cc7efd5c2362ef684657eec7d7e482223b1def4edeb0fab52ba1d334d38a" default))
  '(package-selected-packages
-   '(magit yasnippet-snippets yasnippet quickrun modus-themes use-package)))
+   '(which-key magit yasnippet-snippets yasnippet quickrun modus-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
